25bb6a13c3fb13a7b59b01009e7de57f5f044e66
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2022-11-28 17:24:44 +0100

    added 4 dashboards to dashboards.tf

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -1,6 +1,5 @@
 resource "aws_cloudwatch_dashboard" "main" {
   dashboard_name = var.candidate_id
-## Jim; seriously! we can use any word here.. How cool is that?
   dashboard_body = <<DEATHSTAR
 {
   "widgets": [
@@ -12,15 +11,60 @@ resource "aws_cloudwatch_dashboard" "main" {
       "height": 6,
       "properties": {
         "metrics": [
-          [
-            "${var.candidate_id}",
-            "account_count.value"
-          ]
+          [  "${var.candidate_id}", "carts.value" ]
         ],
-        "period": 300,
-        "stat": "Maximum",
+        "title": "avg amount of carts over at a given time",
         "region": "eu-west-1",
-        "title": "Total number of accounts"
+        "stat": "Average",
+        "period": 5
+      }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 0,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [  "${var.candidate_id}", "cartsvalue.value" ]
+        ],
+        "title": "total amount of chash in all carts at a given time",
+        "region": "eu-west-1",
+        "stat": "Maximum",
+        "period": 5
+      }
+    },
+    {
+      "type": "metric",
+      "x": 0,
+      "y": 6,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [  "${var.candidate_id}", "carts.value" ]
+        ],
+        "title": "total amount of carts in one hour",
+        "region": "eu-west-1",
+        "stat": "Sum",
+        "period": 3600
+      }
+    },
+    {
+      "type": "metric",
+      "x": 12,
+      "y": 6,
+      "width": 12,
+      "height": 6,
+      "properties": {
+        "metrics": [
+          [  "${var.candidate_id}", "checkout_latency.avg", "exception", "none", "method", "checkout", "class", "no.shoppifly.ShoppingCartController" ]
+        ],
+        "title": "avarege checkout latency",
+        "region": "eu-west-1",
+        "stat": "Average",
+        "period": 60
       }
     }
   ]
