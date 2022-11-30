b6027743f0cb8682528ce1f262c64f711c371f54
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2022-11-30 21:27:05 +0100

    commented out appRunner.tf and added .dockerignore file

--- infra/appRunner.tf
+++ infra/appRunner.tf
@@ -1,19 +1,22 @@
-resource "aws_apprunner_service" "service" {
-  service_name = "${var.candidate_id}-app-runner"
+# this is extra and only to show how to use ECR in a app runner
+# commented out this resource as I did not want overcharge the AWS subscription
 
-  source_configuration {
-
-    authentication_configuration {
-      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
-    }
-
-    image_repository {
-      image_configuration {
-        port = "8080"
-      }
-      image_identifier      = "${aws_ecr_repository.repository.repository_url}:latest"
-      image_repository_type = "ECR"
-    }
-    auto_deployments_enabled = true
-  }
-}
+#resource "aws_apprunner_service" "service" {
+#  service_name = "${var.candidate_id}-app-runner"
+#
+#  source_configuration {
+#
+#    authentication_configuration {
+#      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+#    }
+#
+#    image_repository {
+#      image_configuration {
+#        port = "8080"
+#      }
+#      image_identifier      = "${aws_ecr_repository.repository.repository_url}:latest"
+#      image_repository_type = "ECR"
+#    }
+#    auto_deployments_enabled = true
+#  }
+#}
--- infra/ecrRepository.tf
+++ infra/ecrRepository.tf
@@ -1,3 +1,4 @@
+# this is extra to creating the ECR
 resource "aws_ecr_repository" "repository" {
   name = "${var.candidate_id}-repository"
   image_tag_mutability = "IMMUTABLE"
